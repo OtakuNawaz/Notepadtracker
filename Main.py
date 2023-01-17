2d89f32deef10ce836e9c707aec80587dd5f0a4e
Nawaz S <syyednawaz@gmail.com>
2023-01-17 11:30:23 +0530

    Added/Updated files - ID:9282

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ app=Flask(__name__)
 app.config['SECRET_KEY']="my_super_secret_key"
 
 class noteform(FlaskForm):
-    folder_name=StringField('Name of the Folder')
+    folder_name=StringField('Name of the Folder',validators=[DataRequired()])
     note_name=StringField('Name of the Note',validators=[DataRequired()])
     note_description=TextAreaField('Enter the details',validators=[DataRequired()])
     submit=SubmitField('Submit')
@@ -41,18 +41,45 @@ def add_new_note():
 
         with open(f'{folder_name}\{note_name}.txt','w+') as f:
             f.write(f'{note_description}')
+
         flash('New Note Created Successfully')
         git_functions() 
 
     return render_template('add_note.html',form=form,note_name=note_name,note_description=note_description,folder_name=folder_name)
 
-# @app.route('/editfilesfolder')
-# def editfilesfolder():
-#     form=noteform()
-#     folder_name=None
-#     if form.validate_on_submit():
-#         folder_name=form.folder_name.data
-#         form.folder_name.data=''
-#     return render_template('Edit_files_folder.html',form=form,folder_name=folder_name)
+@app.route('/editfilesfolder',methods=['GET','POST'])
+def editfilesfolder():
+    form=noteform()
+    note_name=None
+    folder_name=None
+    note_name=form.note_name.data
+    folder_name=form.folder_name.data
+    if request.method == 'POST':
+        note = note_name + '.txt'
+        if note in os.listdir(folder_name):
+            return redirect(url_for('editdescription',note_name = note_name,folder_name = folder_name))
+        else:
+            return render_template('File_not_found.html',note_name=note_name)
+
+    return render_template('Edit_files_folder.html',form=form,note_name=note_name,folder_name=folder_name)
+
+@app.route('/editfilesfolder/description/<note_name>,<folder_name>',methods=['GET','POST'])
+def editdescription(note_name,folder_name):
+    form=noteform()
+    note_description=None
+    form.note_name.data=''
+    form.folder_name.data=''
+    filepath='\\'.join([folder_name,note_name])
+    with open('{}.txt'.format(filepath),'r') as f:
+        f=f.read()
+    if request.method == 'POST':
+        note_description=form.note_description.data
+        form.note_description.data=''
+        with open(f'{folder_name}\{note_name}.txt','w+') as f:
+            f.write(f'{note_description}')
+        flash('File Updated Successfully')
+        git_functions()
+    return render_template('edit_files_description.html',form=form,f='check',note_description=note_description)
+
 if __name__=="__main__":
     app.run(debug=True)
